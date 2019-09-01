9f411f516a222e814ad65ea574dea090f30c7328
qufwnfahs <style_55@naver.com>
2019-09-01 22:41:44 +0900

    Add function handleProfile()

--- index.js
+++ index.js
@@ -11,6 +11,12 @@ function handleHome(req, res) {
     res.send("Welcome Home!");
 }
 
+function handleProfile(req, res) {
+    res.send("You're on my profile");
+}
+
 app.get("/", handleHome);
+
+app.get("/profile", handleProfile);
 
 app.listen(PORT, handleListening);
