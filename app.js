7fa337f9429fa7e28dde1227195bef808017d3b7
qufwnfahs <style_55@naver.com>
2019-09-03 21:45:58 +0900

    Rename index.js to app.js and Separate functions by use export command
    기존 index.js에서 init 부분을 별개의 코드로 분리
    export를 사용하여 외부의 코드에서 다른 코드 안의 object 사용
    
    - export default `object`
    다른 코드에서 import 시
    import `object` from "./jsFile"
    
    - export const `object`
    다른 코드에서 import 시
    import { `object` } from "./jsFile"

--- app.js
+++ app.js
@@ -5,16 +5,8 @@ import bodyParser from "body-parser";
 import cookieParser from "cookie-parser";
 const app = express();
 
-const PORT = 4000;
-
-const handleListening = () => 
-    console.log(`Listening on localhost:${PORT}`);
-
 const handleHome = (req, res) => 
     res.send("Welcome Home!");
-
-const handleProfile = (req, res) => 
-    res.send("You're on my profile");
 
 // MiddleWares
 app.use(cookieParser());
@@ -25,6 +17,4 @@ app.use(helmet());
 
 app.get("/", handleHome);
 
-app.get("/profile", handleProfile);
-
-app.listen(PORT, handleListening);
+export default app;
