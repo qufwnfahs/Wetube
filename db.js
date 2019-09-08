e594c2e23e7173d8e85e5a4da8089582921ba66d
qufwnfahs <style_55@naver.com>
2019-09-08 20:21:44 +0900

    Add db.js for Fake Database and Show on Home

---
+++ db.js
@@ -0,0 +1,50 @@
+export const videos = [
+    {
+        id: 2345234,
+        title: "Video 1",
+        description: "Test Video 1",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1001,
+            name: "Shinco",
+            email: "Shinco@n.com"
+        }
+    },
+    {
+        id: 12342,
+        title: "Video 2",
+        description: "Test Video 2",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1001,
+            name: "Shinco",
+            email: "Shinco@n.com"
+        }
+    },
+    {
+        id: 123412343,
+        title: "Video 3",
+        description: "Test Video 3",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1001,
+            name: "Shinco",
+            email: "Shinco@n.com"
+        }
+    },
+    {
+        id: 334,
+        title: "Video 4",
+        description: "Test Video 4",
+        views: 24,
+        videoFile: "https://archive.org/details/BigBuckBunny_124",
+        creator: {
+            id: 1001,
+            name: "Shinco",
+            email: "Shinco@n.com"
+        }
+    }
+]
--- routes.js
+++ routes.js
@@ -15,8 +15,8 @@ const CHANGE_PASSWORD ="/change-password";
 const VIDEOS = "/videos";
 const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
-const EDIT_VIDEO = "/edit";
-const DELETE_VIDEO = "/delete";
+const EDIT_VIDEO = "/:id/edit";
+const DELETE_VIDEO = "/:id/delete";
 
 const routes = {
     // Global
