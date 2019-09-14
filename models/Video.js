348ca06ef5a048e938ee45d915568b5915495573
qufwnfahs <style_55@naver.com>
2019-09-14 14:44:51 +0900

    Add Model Schema on MongoDB

---
+++ models/Video.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const VideoSchema = new mongoose.Schema({
+    fileUrl: {
+        type: String,
+        required: "File URL is required"
+    },
+    title: {
+        type: String,
+        required: "Title is required"
+    },
+    description: String,
+    views: {
+        type: Number,
+        default: 0
+    },
+    createdAt: {
+        type: Date,
+        default: Date.new
+    }
+})
+
+const model = mongoose.model("Video", VideoSchema);
+export default model;
