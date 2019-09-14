c4f48800be09e54a68fc7e28f03e3814f8c6c219
qufwnfahs <style_55@naver.com>
2019-09-14 14:24:46 +0900

    Add .env for hiding keys

--- init.js
+++ init.js
@@ -1,7 +1,10 @@
 import app from "./app";
 import "./db";
+import dotenv from "dotenv";
 
-const PORT = 4000;
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
 
 const handleListening = () => {
     console.log(`Listening on http://localhost:${PORT}`);
